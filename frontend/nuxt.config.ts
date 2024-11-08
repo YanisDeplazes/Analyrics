4892486a8304582c02a73f0f763cac45c793db0a
BurkiWorld <marcoburkhardt06@gmail.com>
2024-11-08 14:15:40 +0100

    scss color and typo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
   compatibilityDate: '2024-04-03',
   app: {
     baseURL: "/stuwe1/frontend"
   },
+  
   devtools: { enabled: true }
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint aliquam,
-      assumenda officiis deserunt dolore eligendi commodi fugit nulla tenetur
-      hic veniam sapiente sunt quod quasi. Quo, ducimus! Dolor, necessitatibus
-      alias!
-    </p>
+    <h1 class="font-branding">
+      Sixty zippers were quickly picked from the woven jute bag
+    </h1>
+    <h1>Sixty zippers were quickly picked from the woven jute bag</h1>
+    <h2>Sixty zippers were quickly picked from the woven jute bag</h2>
+    <h3>Sixty zippers were quickly picked from the woven jute bag</h3>
+    <h4>Sixty zippers were quickly picked from the woven jute bag</h4>
+    <h5>Sixty zippers were quickly picked from the woven jute bag</h5>
+    <h6>Sixty zippers were quickly picked from the woven jute bag</h6>
+    <p>Sixty zippers were quickly picked from the woven jute bag</p>
+    <small>Sixty zippers were quickly picked from the woven jute bag</small>
 
     <template>
       <button></button>
