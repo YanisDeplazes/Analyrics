2136a8c990358ed4dccb84555d665441b92614e6
Yanis Deplazes <yanisdeplazes@gmail.com>
2024-11-14 22:43:38 +0100

    add login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h1>Logging in</h1>
-    <a href="http://localhost:3000/login">Log in</a>
-  </div>
+  <Section>
+    <div>
+      <h1 class="display-2">Get a personalised experience</h1>
+      <p>
+        Log in with your Spotify account to get song recommendations tailored
+        just for you! Prefer not to log in? No worries—you can still search and
+        analyze any song lyrics you like.
+      </p>
+      <div class="buttons">
+        <Button text="Log in" variant="secondary" fill="fill" icon="right">
+          <template v-slot:icon>
+            <Icon size="large" variant="arrow-forward"></Icon>
+          </template>
+        </Button>
+        <Button text="Skip" variant="secondary" fill="outline" icon="right">
+          <template v-slot:icon>
+            <Icon size="large" variant="arrow-forward"></Icon>
+          </template>
+        </Button>
+      </div>
+    </div>
+  </Section>
 </template>
+
+<style lang="scss">
+.buttons {
+  display: flex;
+  gap: var(--spacing-lg);
+  .button {
+    width: 100%;
+  }
+}
+</style>
