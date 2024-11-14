4705222ffd64cc05285d6223054c97bce6f02ac9
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-11-14 22:30:49 +0100

    add icon component

---
+++ frontend/components/icon.vue
@@ -0,0 +1,64 @@
+<template>
+    <span :class="{ 'icon-large': size == 'large', 'icon-xl': size == 'xl' }">
+        <img src="~assets/icons/account.svg" v-if="variant == 'account'" />
+        <img src="~assets/icons/arrow-backward.svg" v-if="variant == 'arrow-backward'" />
+        <img src="~assets/icons/arrow-forward.svg" v-if="variant == 'arrow-forward'" />
+        <img src="~assets/icons/check.svg" v-if="variant == 'check'" />
+        <img src="~assets/icons/cross.svg" v-if="variant == 'cross'" />
+        <img src="~assets/icons/error.svg" v-if="variant == 'error'" />
+        <img src="~assets/icons/keyboard-arrow-down.svg" v-if="variant == 'keyboard-arrow-down'" />
+        <img src="~assets/icons/keyboard-arrow-left.svg" v-if="variant == 'keyboard-arrow-left'" />
+        <img src="~assets/icons/keyboard-arrow-right.svg" v-if="variant == 'keyboard-arrow-right'" />
+        <img src="~assets/icons/keyboard-arrow-up.svg" v-if="variant == 'keyboard-arrow-up'" />
+        <img src="~assets/icons/login.svg" v-if="variant == 'login'" />
+        <img src="~assets/icons/lyrics.svg" v-if="variant == 'lyrics'" />
+        <img src="~assets/icons/pause.svg" v-if="variant == 'pause'" />
+        <img src="~assets/icons/play.svg" v-if="variant == 'play'" />
+        <img src="~assets/icons/playlist.svg" v-if="variant == 'playlist'" />
+        <img src="~assets/icons/search.svg" v-if="variant == 'search'" />
+        <img src="~assets/icons/user.svg" v-if="variant == 'user'" />
+        </span>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    size: "large" | "xl",
+    variant: "account"
+    | "arrow-backward"
+    | "arrow-forward"
+    | "check"
+    | "cross"
+    | "error"
+    | "keyboard-arrow-down"
+    | "keyboard-arrow-left"
+    | "keyboard-arrow-right"
+    | "keyboard-arrow-up"
+    | "login"
+    | "lyrics"
+    | "pause"
+    | "play"
+    | "playlist"
+    | "search"
+    | "user"
+}>();
+</script>
+<style lang="css" scoped>
+span{
+    display: inline-block;
+}
+img{
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+}
+.icon-large {
+    margin: var(--spacing-sm);
+    width: 1rem;
+    height: 1rem;
+}
+
+.icon-xl {
+    margin: var(--spacing-md);
+    width: 2rem;
+    height: 2rem;
+}
+</style>
