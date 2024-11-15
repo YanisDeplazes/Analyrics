819cb475c3a3d8b69b0c3054a1a256805a04bbe2
Raika <dgnika@gmail.com>
2024-11-15 14:17:34 +0100

    Reapply "Notification Buttons final. Why does the font look so smashed?"
    This reverts commit 843a57ece8f9f58096b035ed0971d4b99e9232a9.

---
+++ frontend/pages/notification.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Notification type="success" title="Success" description="This is a longer description of why this success notification has appeared." />
+      <Notification type="error" title="Error" description="This is a longer description of why this error notification has appeared." />
+      <Notification type="info" title="Info" description="This is a longer description of why this info notification has appeared." />
+      <Notification type="warning" title="Warning" description="This is a longer description of why this warning notification has appeared." />
+    </div>
+  </template>
+  
+  <script>
+  import Notification from './components/notification.vue';
+  
+  export default {
+    components: {
+      Notification,
+    },
+  };
+  </script> 
