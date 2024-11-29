ee09a42795e166ad5bd1e070585c4bc8dca008af
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-11-29 16:40:36 +0100

    add error handling to analysis in progress

--- frontend/pages/analysis-in-progress.vue
+++ frontend/pages/analysis-in-progress.vue
@@ -8,8 +8,12 @@
       }}
       is being made by {{ store.selectedCritic?.name }}
     </p>
-    <Loader :is-loading="!areLyricsFetched" loading-in-progress-message="Fetching lyrics..." loading-finished-message="Lyrics fetched" ></Loader>
-    <Loader v-if="areLyricsFetched" :is-loading="!areLyricsAnalysed" loading-in-progress-message="Analysing lyrics..." loading-finished-message="Lyrics analysed"></Loader>
+    <Loader :is-loading="!areLyricsFetched" loading-in-progress-message="Fetching lyrics..."
+      loading-finished-message="Lyrics fetched" :has-error="hasLyricsFetchingError"
+      :error-message="lyricsFetchingError"></Loader>
+    <Loader v-if="areLyricsFetched && !hasLyricsFetchingError" :is-loading="!areLyricsAnalysed"
+      loading-in-progress-message="Analysing lyrics..." loading-finished-message="Lyrics analysed"
+      :has-error="hasLyricsAnalysisError" :error-message="lyricsAnalysisError"></Loader>
     <CriticConversation :critic="store.selectedCritic" chat="Okay let's have a look" v-if="store.selectedCritic">
     </CriticConversation>
   </div>
@@ -21,16 +25,33 @@ import { store } from "~/stores/store";
 const backend = new Backend();
 const trackLyrics = ref<TrackLyrics | null>();
 const areLyricsFetched = ref<boolean>(false);
+const hasLyricsFetchingError = ref<boolean>(false);
+const lyricsFetchingError = ref<string>("");
 const areLyricsAnalysed = ref<boolean>(false);
+const hasLyricsAnalysisError = ref<boolean>(false);
+const lyricsAnalysisError = ref<string>("");
 onMounted(async () => {
   if (store.selectedTrack && store.selectedCritic) {
-    trackLyrics.value = await backend.lyrics(store.selectedTrack);
-    areLyricsFetched.value = true;
-    store.setLineAnalysis(await backend.analyseLyrics(trackLyrics.value, store.selectedCritic));
-    areLyricsAnalysed.value = true;
-    setTimeout(() => {
-      navigateTo("analysis-result");
-    }, 2000)
+    try {
+      trackLyrics.value = await backend.lyrics(store.selectedTrack);
+      areLyricsFetched.value = true;
+    }
+    catch (error) {
+      hasLyricsFetchingError.value = true;
+      lyricsFetchingError.value = "This is embarrassing, but we could not get the lyrics for this track.";
+      return;
+    }
+    try {
+      store.setLineAnalysis(await backend.analyseLyrics(trackLyrics.value, store.selectedCritic));
+      areLyricsAnalysed.value = true;
+      setTimeout(() => {
+        navigateTo("analysis-result");
+      }, 2000)
+    }
+    catch (error) {
+      hasLyricsAnalysisError.value = true;
+      lyricsAnalysisError.value = "Well... our AI seems to be too dumb to analyze these lyrics. We sincerely apologize."
+    }
   }
 });
 </script>
