fb0a63b7dff65f6d09b354e5383482f271600ea9
Raika <dgnika@gmail.com>
2024-11-29 13:59:48 +0100

    Installed rmocha reporter for cypress and dependecies als wrote code to generate report

--- frontend/cypress/e2e/loginflow.cy.js
+++ frontend/cypress/e2e/loginflow.cy.js
@@ -1,10 +1,13 @@
 // Arrange, Act, Assert
 
 describe('Login flow test', () => {
-  it('Starts up the index page', () => {
+  it('Starts up the index page', { tags: ['login', 'critical'] }, () => {
     cy.visit('http://localhost:8888/stuwe1/frontend/')
+
     // Is the button visible?
-    cy.get('button').contains('Explore now').should('be.visible');
+    cy.get('button')
+    .contains('Explore now')
+    .should('be.visible');
 
     // Can I click on the button and will the login-page open up?
     cy.contains('Explore now').click();
