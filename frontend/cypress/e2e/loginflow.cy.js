7de39a529804a24db79b36de19f3266017d4cb1a
Raika <dgnika@gmail.com>
2024-11-29 11:06:24 +0100

    e2e test 1 and setup of test 2
    Made it work now

--- frontend/cypress/e2e/loginflow.cy.js
+++ frontend/cypress/e2e/loginflow.cy.js
@@ -1,17 +1,13 @@
 // Arrange, Act, Assert
 
-describe('Index page test', () => {
+describe('Login flow test', () => {
   it('Starts up the index page', () => {
     cy.visit('http://localhost:8888/stuwe1/frontend/')
-  })
-
-  it('should display the "Explore Now" button and if clickable', () => {
     // Is the button visible?
     cy.get('button').contains('Explore now').should('be.visible');
 
     // Can I click on the button and will the login-page open up?
     cy.contains('Explore now').click();
     cy.url().should('include', '/login');
-  });
-
-})
+  })
+});
