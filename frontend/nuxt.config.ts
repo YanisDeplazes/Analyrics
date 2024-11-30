b8805395a47a6e7904a9fcd58389ae16a6564a4e
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-11-30 13:53:43 +0100

    move style include to nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+
   compatibilityDate: '2024-04-03',
   app: {
     baseURL: "/stuwe1/frontend"
@@ -8,5 +8,8 @@ export default defineNuxtConfig({
   devServer: {
     port: 8888
   },
+  css: [
+    "~/assets/scss/style.scss"
+  ],
   devtools: { enabled: true }
 })
