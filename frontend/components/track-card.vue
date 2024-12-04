8d23b89c01bf3ce46ef80d9935aed12610c00833
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-12-04 12:15:23 +0100

    add track card

---
+++ frontend/components/track-card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { store } from '~/stores/store';
+</script>
+
+<template>
+    <div class="track-container">
+        <p>Your track</p>
+        <div class="track">
+            <img :src="store.selectedTrack?.album.images[0].url" />
+            <div class="track-info">
+                <p>{{ store.selectedTrack?.name }}</p>
+                <p>{{ commaSeparatedArtists(store.selectedTrack!.artists) }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.track-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: $spacing-md;
+    padding: $spacing-md;
+    border-radius: $spacing-md;
+    background-color: $bg-secondary;
+    color: $on-secondary;
+
+    .track {
+        display: flex;
+        flex-direction: row;
+        gap: $spacing-md;
+
+        img {
+            width: 2.5rem;
+            height: 2.5rem;
+        }
+
+        .track-info {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+
+
+}
+</style>
--- frontend/pages/critic-selection.vue
+++ frontend/pages/critic-selection.vue
@@ -1,13 +1,5 @@
 <template>
   <Section class="critic-selection-section">
-    <p>
-      Nice selection {{ store.spotifyProfile?.display_name ?? "" }}! Which
-      critic should analyse “{{ store.selectedTrack?.name }}” by
-      {{
-        store.selectedTrack &&
-        commaSeparatedArtists(store.selectedTrack?.artists)
-      }}?
-    </p>
     <SwiperWrapper :slides-per-view="1.1" :space-between="8" :modules="[Scrollbar, FreeMode]" :loop="true"
       :scrollbar="{ draggable: true }" :breakpoints="{
         480: {
@@ -33,6 +25,7 @@
         </Critic>
       </swiper-slide>
     </SwiperWrapper>
+    <TrackCard></TrackCard>
   </Section>
 </template>
 <script setup lang="ts">
@@ -56,6 +49,7 @@ const setCritic = (critic: Critic) => {
 .critic-selection-section {
   display: flex;
   flex: 1;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
