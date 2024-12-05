b6a35bcd319f1c27413b7c4116ceb41ada331087
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-12-05 15:47:07 +0100

    Refine disclaimer text

---
+++ frontend/components/disclaimer.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2>Disclaimer</h2>
+    <p>
+        The application uses a Large Language Model by Google called Gemini to
+        analyse song lyrics. The lyrics are provided by Genius.com, where most
+        lyrics are user-submitted; therefore, complete accuracy cannot be
+        guaranteed. Please note that the analyses provided are for informational
+        purposes only. We disclaim any responsibility for the accuracy or
+        completeness of the lyrics or their interpretations. The analysis may not
+        fully capture the intended meaning or context of the lyrics. For critical
+        interpretations and insights, we recommend consulting the original source
+        or a lyricist directly.
+    </p>
+</template>
+<style lang="scss" scoped>
+p {
+    column-count: 1;
+    gap: $spacing-lg;
+
+    @include lg {
+        column-count: 2;
+    }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -11,13 +11,7 @@
       Collective offers a fresh perspective on every song.
     </p>
     <NuxtLink to="/login">
-      <Button
-        text="Explore now"
-        variant="secondary"
-        fill="fill"
-        size="large"
-        icon="right"
-      >
+      <Button text="Explore now" variant="secondary" fill="fill" size="large" icon="right">
         <template v-slot:icon>
           <Icon size="large" icon="arrow-forward" variant="secondary"></Icon>
         </template>
@@ -28,18 +22,7 @@
     <CrititsOverview />
   </Section>
   <Section class="disclaimer-section">
-    <h2>Disclaimer</h2>
-    <p>
-      The application uses a Large Language Model by Google called Gemini to
-      analyse song lyrics. The lyrics are provided by Genius.com, where most
-      lyrics are user-submitted; therefore, complete accuracy cannot be
-      guaranteed. Please note that the analyses provided are for informational
-      purposes only. We disclaim any responsibility for the accuracy or
-      completeness of the lyrics or their interpretations. The analysis may not
-      fully capture the intended meaning or context of the lyrics. For critical
-      interpretations and insights, we recommend consulting the original source
-      or a lyricist directly.
-    </p>
+    <Disclaimer />
   </Section>
 </template>
 
