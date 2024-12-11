9053e43ff3f1c44dd8b861129026b855271e910f
stefanvoegelisrf <stefan.voegeli@srf.ch>
2024-12-11 12:29:48 +0100

    add lyrics test page

---
+++ frontend/pages/lyrics-test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="lyrics-container">
+        <p>Sad - Demi Lovato - Sober</p>
+        <h1 class="font-sad">I don't know, I don't know, I don't know, I don't know why<br />I do it every, every, every
+            time</h1>
+        <p>Weird - Money Boy - Monte Carlo</p>
+        <h1 class="font-weird">Meine Chain sieht aus, als wären Glühbirnen drauf (Ice)<br />Zitroneneiscreme in meinen
+            Ohren und im Mouth (Burr)</h1>
+        <p>Happy - Jul - Mon Bijou</p>
+        <h1 class="font-happy">Quand tu m'serres dans tes bras<br />J'te veux présente dans les hauts et les bas</h1>
+        <p>Smirk - Lorenzo - Bizarre</p>
+        <h1 class="font-smirk">C'pas demain qu'on fera d'la lèche<br />Gros popotin, yeux de biche</h1>
+        <p>Calm - Akua Naru - Poetry: How Does It Feel Now???</p>
+        <h1 class="font-calm">My, I wanna drink the sweat of your intellect<br />
+            Reflect, and watch your light passion mark my neck
+        </h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+.lyrics-container {
+    display: flex;
+    flex-direction: column;
+    gap: $spacing-lg;
+}
+</style>
